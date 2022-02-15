5ab9c39e8f8b3c36f483d8eca194e917f087caff
pillow <fard.cca@gmail.com>
2022-02-15 17:39:37 +0100

    Fix getDb, fix install.js

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -1,7 +1,9 @@
 import { openDB } from "idb";
+import { header } from "./header";
 
 const dbName = "jate";
 const storeName = "jate";
+const contentKey = 1;
 
 const initdb = async () =>
   openDB("jate", 1, {
@@ -20,9 +22,18 @@ const initdb = async () =>
 export const putDb = async (content) => {
   const db = await openDB(dbName);
   const store = db.transaction(storeName, "readwrite").objectStore(storeName);
+  await store.put({ content, id: contentKey });
 };
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error("getDb not implemented");
+export const getDb = async () => {
+  const db = await openDB(dbName);
+  const store = db.transaction(storeName, "readwrite").objectStore(storeName);
+  const data = await store.get(contentKey);
+  if (!data) {
+    return header;
+  }
+  return data.content;
+};
 
 initdb();
--- Develop/client/src/js/install.js
+++ Develop/client/src/js/install.js
@@ -2,10 +2,19 @@ const butInstall = document.getElementById("buttonInstall");
 
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener("beforeinstallprompt", (event) => {});
-
-// TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener("click", async () => {});
+window.addEventListener("beforeinstallprompt", (event) => {
+  event.preventDefault();
+  butInstall.style.visibility = "visible";
+  // TODO: Implement a click event handler on the `butInstall` element
+  butInstall.addEventListener("click", () => {
+    event.prompt();
+    console.log("Intall the application");
+    butInstall.setAttribute("disabled", true);
+  });
+});
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener("appinstalled", (event) => {});
+window.addEventListener("appinstalled", (event) => {
+  butInstall.style.visibility = "hidden";
+  console.log("App is installed successfully");
+});
