b990625ef74b12b1a4680725a7d2ad6fbe5c318c
pillow <fard.cca@gmail.com>
2022-02-16 18:13:33 +0100

    Add index route for testing heroku

--- server/server.js
+++ server/server.js
@@ -1,12 +1,17 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static('../client/dist'));
+app.use(express.static("../client/dist"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-require('./routes/htmlRoutes')(app);
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "../client/dist/index.html"))
+);
+app.get("/index", (req, res) =>
+  res.sendFile(path.join(__dirname, "../client/dist/index.html"))
+);
 
 app.listen(PORT, () => console.log(`Now listening on port: ${PORT}`));
