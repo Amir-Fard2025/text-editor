b7db0c569b37db8f52ab3f7071497be689f93f42
pillow <fard.cca@gmail.com>
2022-02-17 18:05:09 +0100

    Fix the visibility of install button in install.js

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -3,18 +3,33 @@ const butInstall = document.getElementById("buttonInstall");
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener("beforeinstallprompt", (event) => {
-  event.preventDefault();
-  butInstall.style.visibility = "visible";
-  // TODO: Implement a click event handler on the `butInstall` element
-  butInstall.addEventListener("click", () => {
-    event.prompt();
-    console.log("Intall the application");
-    butInstall.setAttribute("disabled", true);
-  });
+  // event.preventDefault();
+  window.deferredPrompt = event;
+  // butInstall.style.visibility = "visible";
+  butInstall.classList.toggle("hidden", false);
+});
+
+// TODO: Implement a click event handler on the `butInstall` element
+butInstall.addEventListener("click", () => {
+  // event.prompt();
+  // console.log("Intall the application");
+  const promptEvent = window.deferredPrompt;
+
+  if (!promptEvent) {
+    return;
+  }
+  promptEvent.prompt();
+
+  window.deferredPrompt = null;
+
+  butInstall.classList.toggle("hidden", true);
+  // butInstall.setAttribute("disabled", true);
 });
 
 // TODO: Add an handler for the `appinstalled` event
 window.addEventListener("appinstalled", (event) => {
-  butInstall.style.visibility = "hidden";
-  console.log("App is installed successfully");
+  // event.preventDefault();
+  // butInstall.style.visibility = "hidden";
+  window.deferredPrompt = null;
+  console.log("App is installed successfully", event);
 });
